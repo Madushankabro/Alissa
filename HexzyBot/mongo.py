9d7385d2ef27494f6a9372e0dc0a5f4c8b518376
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-06-14 16:50:00 +0530

    Update mongo.py

--- HexzyBot/mongo.py
+++ HexzyBot/mongo.py
@@ -10,7 +10,7 @@ from HexzyBot.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "Lyci"
+MONGO_DB = "DaisyX"
 
 
 client = MongoClient()
