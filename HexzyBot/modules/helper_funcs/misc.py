2d9e6d8a777b4226b06d0aa8c4e2b3cffbf73ff3
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-06-15 20:07:51 +0530

    Update misc.py

--- HexzyBot/modules/helper_funcs/misc.py
+++ HexzyBot/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="layla_back"),
+                EqInlineKeyboardButton("Home🏘", callback_data="hexzy_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
+        pairs += [[EqInlineKeyboardButton("Home🏘", callback_data="hexzy_back")]]
 
     return pairs
 
