6d317583ced15c6a0edc9335d65b02bc9926025b
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-06-16 07:06:30 +0530

    Update events.py

--- HexzyBot/pyrogramee/events.py
+++ HexzyBot/pyrogramee/events.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 from telethon import events
 
-from DaisyX.services.mongo import mongodb as db
-from DaisyX.services.telethon import tbot
+from HexzyBot.pyrogramee.mongo import mongodb as db
+from HexzyBot.pyrogramee.telethon import tbot
 
 gbanned = db.gban
 CMD_LIST = {}
