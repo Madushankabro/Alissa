ea2e1ec257adb1af9ffd3c480345b1e6a4ae0ead
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-06-17 10:28:26 +0530

    Update __init__.py

--- HexzyBot/__init__.py
+++ HexzyBot/__init__.py
@@ -67,6 +67,7 @@ if ENV:
     PORT = int(os.environ.get("PORT", 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
     API_ID = os.environ.get("API_ID", None)
+    APP_ID = os.environ.get("APP_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     BOT_ID = int(os.environ.get("BOT_ID", None))
     DB_URI = os.environ.get("DATABASE_URL")
