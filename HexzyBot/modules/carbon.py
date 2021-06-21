479ef1e1c590de2c9dc0f26ee261f95a03b637af
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-06-21 08:07:23 +0530

    Update carbon.py

--- HexzyBot/modules/carbon.py
+++ HexzyBot/modules/carbon.py
@@ -1,3 +1,12 @@
+import os
+
+from pyrogram import filters
+
+from HexzyBot import app
+from HexzyBot.core.decorators.errors import capture_err
+from HexzyBot.utils.functions import make_carbon
+
+
 __mod_name__ = "⚡️Carbon⚡️"
 
 __help__ = """
@@ -6,3 +15,21 @@ __help__ = """
 Commands
  ❍ /carbon : Reply to a text message to make carbon.
 """
+
+@app.on_message(filters.command("carbon"))
+@capture_err
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply to a text message to make carbon."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "Reply to a text message to make carbon."
+        )
+    m = await message.reply_text("Preparing Carbon")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("Uploading")
+    await app.send_document(message.chat.id, carbon)
+    await m.delete()
+    carbon.close()
