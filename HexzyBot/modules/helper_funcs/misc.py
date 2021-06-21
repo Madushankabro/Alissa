c515a99b70e94ac17e00178d09f508550aee41a5
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-06-21 07:40:47 +0530

    Update misc.py

--- HexzyBot/modules/helper_funcs/misc.py
+++ HexzyBot/modules/helper_funcs/misc.py
@@ -64,8 +64,8 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 8:
-        pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
+    if len(pairs) > 10:
+        pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("Home🏘", callback_data="hexzy_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
